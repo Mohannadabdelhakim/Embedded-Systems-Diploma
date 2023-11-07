3edc68a00c1ff67d38cc4fe8e8bb4e873d58ce86
Mohannadabdelhakim <142653232+Mohannadabdelhakim@users.noreply.github.com>
2023-11-07 13:10:53 +0200

    Add files via upload

---
+++ even/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int i=0;
+    printf("please enter a number \n");
+    scanf(" %d",&i);
+    if(i%2==0)
+    {
+        printf("\n the number is even \n");
+    }
+    else
+    {
+        printf("\n the number is odd \n");
+
+    }
+    return 0;
+}
+
