d683745ad3bd1f8c5c403a8c8841be3626bb5c85
Mohannadabdelhakim <142653232+Mohannadabdelhakim@users.noreply.github.com>
2024-04-10 06:44:21 +0200

    Add files via upload

---
+++ LCD_KEYPAD_driver/LCD_KEYPAD_driver/keypad/keypad.h
@@ -0,0 +1,34 @@
+/*
+ * keypad.h
+ *
+ * Created: 4/10/2024 
+ *  Author: Mohannad Abd Elhakim
+ */ 
+
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+#define F_CPU 8000000UL
+
+#include<avr/io.h>
+#include<util/delay.h>
+
+
+#define R0 0
+#define R1 1
+#define R2 2
+#define R3 3
+#define C0 4
+#define C1 5
+#define C2 6
+#define C3 7
+
+#define KEYPAD_PORT PORTD
+#define DataDir_KEYPAD_PORT DDRD
+#define keypadPIN PIND
+
+void Keypad_init();
+char Keypad_getkey();
+
+#endif /* KEYPAD_H_ */
